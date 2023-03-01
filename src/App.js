03e08d4c223a98d7357e6d9135a4f99c42147306
unknown <lynda@uzuriky.com>
2023-03-01 16:47:05 +0200

    fetch api data and search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      value: "",
+      inputSearch: "",
+      temp: { list: [] }
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?q=kigali&appid=1383c1ef39cccbbc106d269850cfc481"
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ temp: data, isLoading: false, inputSearch: "" });
+      });
+  }
+
+  handlerEventInput = e => {
+    const inputSearch = e.target.value;
+    this.setState({ inputSearch });
+  };
+
+  render() {
+    const { inputSearch } = this.state;
+
+    const searchInputDisplay = this.state.temp.list.filter(data => {
+      return data.weather[0].description
+        .toLowerCase()
+        .includes(inputSearch.toLowerCase());
+    });
+
+    return this.state.isLoading
+      ? <h1 className="loading">is loading.......</h1>
+      : <div className="app-container">
+          <input type="search" onChange={this.handlerEventInput} />
+
+          <p>
+            {searchInputDisplay.map(list => {
+              return (
+                <p key={list.dt}>
+                  {list.weather[0].description.toLowerCase()}
+                </p>
+              );
+            })}
+          </p>
+        </div>;
+  }
 }
 
 export default App;
